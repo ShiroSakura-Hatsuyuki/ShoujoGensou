7a508d1ecd88430334400866e44cb04867484549
ShiroSakura-Hatsuyuki <200654583+ShiroSakura-Hatsuyuki@users.noreply.github.com>
2026-05-29 18:17:25 +0800

    在 SPM 里添加 Yukari2SCaeKriMoYhEn7 目标

--- YukariYuyuko/Package.swift
+++ YukariYuyuko/Package.swift
@@ -4,11 +4,35 @@ import PackageDescription
 
 let package = Package(
     name: "YukariYuyuko",
+    products:[
+        .executable(
+            name:"YukariYuyuko",targets:["YukariYuyuko"]
+        )
+    ],
     targets: [
         .executableTarget(
             name: "YukariYuyuko",
-            path: "Source/YukariYuyuko"
-        )
+            dependencies: [
+                .target(name: "Yukari2SCaeKriMoYhEn7")
+
+            ],
+            path: "Source/YukariYuyuko",
+            swiftSettings: [
+                .interoperabilityMode(.Cxx)
+            ]
+        ),
+        .target(
+            name: "Yukari2SCaeKriMoYhEn7", //YukariSakuraShizukuCaedeYuki/YuriMoZomeYoohuaEn 紫樱雫枫雪百合墨染幽华缘
+            dependencies: [
+
+            ],
+            path: "Source/Yukari2SCaeKriMoYhEn7",
+            swiftSettings: [
+                .interoperabilityMode(.Cxx)
+            ]
+        ),
     ],
+
+
     swiftLanguageModes: [.v6]
 )
